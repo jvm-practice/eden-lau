901c62c5ce782a76d9efbef76088f0073437832e
eden-lau <eden-lau@qq.com>
2019-12-19 19:17:31 +0800

    JUnit4 Demo
    Resolve #1

--- src/main/java/com/mycompany/app/JunitDemo.java
+++ src/main/java/com/mycompany/app/JunitDemo.java
@@ -1,7 +1,7 @@
 package com.mycompany.app;
 
-public class JunitDemo{
-    public int add(int a, int b){
+public class JunitDemo {
+    public int add(int a, int b) {
         return a + b;
     }
 }
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -7,20 +7,20 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
     }
+
     @Test
-    public void testAdd(){
+    public void testAdd() {
         JunitDemo jd = new JunitDemo();
-        int res = jd.add(2,4);
-        assertEquals(6,res);
+        int res = jd.add(2, 4);
+        assertEquals(6, res);
+        assertNotEquals(8, res);
     }
 }
